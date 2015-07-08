83e7e707921f1edb296e4f4db2d19721c601e4fa
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-07-09 01:03:41 +0200

    Fix autoloader path

--- activate.php
+++ activate.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once __DIR__ . '/lib/autoloader.php';
+require_once __DIR__ . '/autoloader.php';
 
 update_subtype('object', \hypeJunction\Graph\Consumer::SUBTYPE, \hypeJunction\Graph\Consumer::CLASSNAME);
