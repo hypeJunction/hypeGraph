e9f17071b87dfb80cbb5315bbb13086a875a1430
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-07-27 13:56:25 +0200

    chore(composer): autoloading

--- autoloader.php
+++ autoloader.php
@@ -4,14 +4,6 @@ if (!is_callable('hypeApps')) {
 	throw new Exception("hypeGraph requires hypeApps");
 }
 
-$path = __DIR__;
-
-if (!file_exists("{$path}/vendor/autoload.php")) {
-	throw new Exception('hypeGraph can not resolve composer dependencies. Run composer install');
-}
-
-require_once "{$path}/vendor/autoload.php";
-
 /**
  * Plugin container
  * @return \hypeJunction\Graph\Plugin
