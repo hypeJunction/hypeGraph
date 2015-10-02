e9539dc1b7b7d65998d46ef4e6b2877dc9e48c7d
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-10-02 16:49:09 +0300

    chore(export): adapt export results

--- classes/hypeJunction/Graph/BatchResult.php
+++ classes/hypeJunction/Graph/BatchResult.php
@@ -8,10 +8,21 @@ namespace hypeJunction\Graph;
 class BatchResult extends \hypeJunction\BatchResult {
 	
 	/**
-	 * {@inheritdoc}
+	 * Export batch into an array
+	 *
+	 * @param array  $fields    Fields to export
+	 * @param bool   $recursive Export owners and containers recursively
+	 * @param string $key       Parameter name to hold batch item export
+	 * @return stdClass
 	 */
-	public function export($fields = array(), $key = 'nodes') {
+	public function export($fields = array(), $recursive = true, $key = 'nodes') {
+
 		hypeGraph()->logger->log(array('BatchOptions' => $this->prepareBatchOptions($this->options)));
-		return parent::export($fields, $key);
+
+		$result = (object) parent::export($fields, $recursive, $key);
+		$result->total = $result->count;
+		unset($result->count);
+
+		return $result;
 	}
 }
