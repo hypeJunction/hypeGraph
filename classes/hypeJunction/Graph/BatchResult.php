201f4ed75ff9acc8ec41f6a151bc2f252caf6e45
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-10-03 10:29:39 +0300

    fix(graph): update the use of export params

--- classes/hypeJunction/Graph/BatchResult.php
+++ classes/hypeJunction/Graph/BatchResult.php
@@ -10,16 +10,17 @@ class BatchResult extends \hypeJunction\BatchResult {
 	/**
 	 * Export batch into an array
 	 *
-	 * @param array  $fields    Fields to export
-	 * @param bool   $recursive Export owners and containers recursively
-	 * @param string $key       Parameter name to hold batch item export
+	 * @param array $params Export params
 	 * @return stdClass
 	 */
-	public function export($fields = array(), $recursive = true, $key = 'nodes') {
+	public function export(array $params = array()) {
 
 		hypeGraph()->logger->log(array('BatchOptions' => $this->prepareBatchOptions($this->options)));
 
-		$result = (object) parent::export($fields, $recursive, $key);
+		$result = (object) parent::export($params);
+		$result->nodes = $result->items;
+		unset($result->items);
+		
 		$result->total = $result->count;
 		unset($result->count);
 
